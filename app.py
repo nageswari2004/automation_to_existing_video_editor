42b72f626640da8c3d9cbffb435dcbd0c35fa4c5
nageswari2004 <maruboina2004.com>
2025-06-20 06:23:00 +0530

    Add gunicorn to requirements for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template, send_from_directory, redirect, url_for, session, flash
-import moviepy.editor as mp
+import moviepy as mp
 import os
 from pathlib import Path
 from werkzeug.utils import secure_filename
